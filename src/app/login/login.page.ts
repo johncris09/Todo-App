f4c0203a468f4776cf885ae8abe335ddca6df04c
johncris09 <jcmanabo@gmail.com>
2019-06-11 16:19:46 +0800

    facebook login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -20,17 +20,28 @@ export class LoginPage implements OnInit {
   ngOnInit() {
   }
  
-  Login(){
+  googleplusLogin(){
   	this.googlePlus.login({
   	  'webClientId': '1047252692996-jch521c4gouebt4l42re3dslb40jasp1.apps.googleusercontent.com',
-  	  'offline' : true,
-
+  	  'offline' : true, 
   	}).then(res=>{
   		firebase.auth().signInWithCredential(firebase.auth.GoogleAuthProvider.credential(res.idToken)).then(suc=>{
   		  console.log("Login Success");
   		}).catch(err=>{
   		  console.log('faile to login');
   		})
+  	});
+  }
+
+  facebookLogin(){ 
+  	let provider = new firebase.auth.FacebookAuthProvider();
+
+  	firebase.auth().signInWithRedirect(provider).then(()=>{
+  	  firebase.auth().getRedirectResult().then((result)=>{
+  	  	alert(JSON.stringify(result));
+  	  }).catch(function(error) {
+  	  	alert(JSON.stringify(error));
+  	  });
   	});
   }
 
